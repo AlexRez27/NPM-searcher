fe0a2b42c56d459aadf2fcf19d28fc1b53c7b768
AlexRez27 <rezident027@gmail.com>
2024-02-08 11:11:20 +0200

    small fixes

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,18 @@
 <template>
   <ContainerComponent>
-    <search-component></search-component>
-    <package-list v-if="store.NPMItems.length > 0" :package-items="store.NPMItems"></package-list>
-    <v-progress-circular
-      v-if="store.isLoaded && store.NPMItems.length === 0"
-      :size="150"
-      color="primary"
-      indeterminate
-    ></v-progress-circular>
-    <!-- <v-alert v-else> No packages found </v-alert> -->
+    <body>
+      <search-component></search-component>
+      <package-list v-if="store.NPMItems.length > 0" :package-items="store.NPMItems"></package-list>
+      <v-progress-circular
+        v-if="store.isLoaded && store.NPMItems.length === 0"
+        :size="150"
+        color="primary"
+        indeterminate
+      ></v-progress-circular>
+    </body>
+    <footer class="d-flex flex-column px-0">
+      <footer-component></footer-component>
+    </footer>
   </ContainerComponent>
 </template>
 
@@ -17,6 +21,6 @@ import SearchComponent from './components/SearchComponent.vue'
 import ContainerComponent from './components/ContainerComponent.vue'
 import PackageList from './components/PackageList.vue'
 import { useNPM } from '@/stores'
-
+import FooterComponent from '@/components/FooterComponent.vue'
 const store = useNPM()
 </script>
--- src/components/ContainerComponent.vue
+++ src/components/ContainerComponent.vue
@@ -1,15 +1,8 @@
 <template>
   <div class="container">
     <slot />
-    <footer class="d-flex flex-column px-0">
-      <footer-component></footer-component>
-    </footer>
   </div>
 </template>
-
-<script setup lang="ts">
-import FooterComponent from './FooterComponent.vue'
-</script>
 
 <style lang="scss" scoped>
 .container {
@@ -17,8 +10,5 @@ import FooterComponent from './FooterComponent.vue'
   flex-direction: column;
   height: 100vh;
   justify-content: space-between;
-  // max-width: 1400px;
-  // margin: 0 auto;
-  // margin-top: 50px;
 }
 </style>
